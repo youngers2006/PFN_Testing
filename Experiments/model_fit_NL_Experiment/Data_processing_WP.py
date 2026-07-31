cbca457d5d5627575037b62f4fd6803bde081c29
youngers2006 <sy2224@ic.ac.uk>
2026-07-31 13:51:56 +0100

    fix

--- Experiments/model_fit_NL_Experiment/Data_processing_WP.py
+++ Experiments/model_fit_NL_Experiment/Data_processing_WP.py
@@ -80,5 +80,5 @@ def compute_comparative_metrics(base_save_dir: str):
 
 if __name__ == "__main__":
     # Substitute with your actual base_save_dir
-    base_dir = "LS_1D_varied_results/run_20260730_130530" 
+    base_dir = "WP_1D_varied_results/run_20260731_120345" 
     compute_comparative_metrics(base_dir)
