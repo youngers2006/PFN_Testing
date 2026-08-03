831ad6d35763552e9f3ec362a1aed23961770b93
youngers2006 <sy2224@ic.ac.uk>
2026-08-03 17:26:44 +0100

    added

--- Experiments/model_fit_kernel_Experiment/Run_Main_Tests_kernels.py
+++ Experiments/model_fit_kernel_Experiment/Run_Main_Tests_kernels.py
@@ -12,7 +12,7 @@ from Model_fit_fns import plot_GP_variance_surface, plot_pfn_variance_surface
 def main():
     # Save paths
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    base_save_dir = f"WP_1D_varied_results/run_{timestamp}"
+    base_save_dir = f"K_1D_varied_results/run_{timestamp}"
     os.makedirs(base_save_dir, exist_ok=True)
 
     # Device
