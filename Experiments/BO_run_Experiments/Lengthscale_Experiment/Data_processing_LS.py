b0fed50400ff181fa2156f2c20507fc9c4988378
youngers2006 <sy2224@ic.ac.uk>
2026-08-08 17:47:13 +0100

    added

--- Experiments/BO_run_Experiments/Lengthscale_Experiment/Data_processing_LS.py
+++ Experiments/BO_run_Experiments/Lengthscale_Experiment/Data_processing_LS.py
@@ -4,34 +4,40 @@ import numpy as np
 from scipy.stats import spearmanr, rankdata
 from tqdm import tqdm
 
-def compute_comparative_metrics(base_save_dir: str):
+def compute_comparative_metrics(base_save_dir_1: str, base_save_dir_2: str):
     # Load experimental results
-    results_path = os.path.join(base_save_dir, "experimental_results.pt")
-    data = torch.load(results_path, map_location='cpu')
+    results_path_1 = os.path.join(base_save_dir_1, "experimental_results.pt")
+    results_path_2 = os.path.join(base_save_dir_2, "experimental_results.pt")
+    data_1 = torch.load(results_path_1, map_location='cpu')
+    data_2 = torch.load(results_path_2, map_location='cpu')
     
     # load minimum of each experiment
-    minimas_path = os.path.join(base_save_dir, "maximas.pt")
+    minimas_path = os.path.join(base_save_dir_1, "maximas.pt")
     minima_data = torch.load(minimas_path, map_location='cpu')
 
     # Extract minimum values
     true_minimas_y = minima_data["y"] # Shape: (3, 3, n_repeats, 1)
 
     # Extract
-    y_true_store = data["y_true"]
-    y_best_store = data["y_best"]
-    mu_store = data["mu"]
+    y_true_store_main = data_1["y_true"]
+    y_best_store_main = data_1["y_best"]
+    mu_store_main = data_1["mu"]
+
+    y_true_store_warp = data_1["y_true"]
+    y_best_store_warp = data_1["y_best"]
+    mu_store_warp = data_1["mu"]
     
     # Data shape extraction
-    x_dims = [2, 5, 10]
-    methods = ["GP", "PFN", "Random"]
+    x_dims = [1]
+    methods = ["GP", "PFN", "Random", "Warped_PFN"]
 
     # Sizing
     n_dims = 1
-    n_methods = 3
+    n_methods = 4
     n_repeats = 21
-    N_iters = 150
+    N_iters = 50
     n_fns = 1
-    n_tests = 7
+    n_tests = 9
     
     # Initialize metric storage
     metrics = {
@@ -62,43 +68,40 @@ def compute_comparative_metrics(base_save_dir: str):
                     # Looping through methods
                     for m_idx in range(n_methods):
                         # Calculate simple regret
-                        y_best_found = y_best_store[k][test, m_idx, rep, t, 0].item()
-                        metrics["regret"][test, k, m_idx, rep, t] = abs(y_opt_continuous - y_best_found)
+                        if m_idx != 3:
+                            y_best_found = y_best_store_main[k][test, m_idx, rep, t, 0].item()
+                            metrics["regret"][test, k, m_idx, rep, t] = abs(y_opt_continuous - y_best_found)
+                        elif m_idx == 3:
+                            y_best_found = y_best_store_warp[k][test, 0, rep, t, 0].item()
+                            metrics["regret"][test, k, m_idx, rep, t] = abs(y_opt_continuous - y_best_found)
                         
                         # Prediction error of surrogate
-                        if m_idx < 2:
-                            queried_y = y_true_store[k][test, m_idx, rep, t, 0].item()
-                            pred_mu = mu_store[k][test, m_idx, rep, t, 0].item()
+                        if m_idx == 0 or m_idx == 1:
+                            queried_y = y_true_store_main[k][test, m_idx, rep, t, 0].item()
+                            pred_mu = mu_store_main[k][test, m_idx, rep, t, 0].item()
+                            metrics["pred_error"][test, k, m_idx, rep, t] = abs(pred_mu - queried_y)
+                        elif m_idx == 3:
+                            queried_y = y_true_store_warp[k][test, 0, rep, t, 0].item()
+                            pred_mu = mu_store_warp[k][test, 0, rep, t, 0].item()
                             metrics["pred_error"][test, k, m_idx, rep, t] = abs(pred_mu - queried_y)
                     
                     # Rank between methods
-                    y_t_vals = y_true_store[k][test, :, rep, t, 0].numpy()
+                    y_t_vals_main = y_true_store_main[k][test, :, rep, t, 0].numpy()
+                    y_t_vals_warp = y_true_store_warp[k][test, :, rep, t, 0].numpy()
+                    y_t_vals = np.concatenate([y_t_vals_main, y_t_vals_warp])
+
                     # Negate y_t_vals so the highest objective gets rank 1
                     ranks = rankdata(-y_t_vals, method='min') 
                     for m_idx in range(n_methods):
                         metrics["rank_of_method"][test, k, m_idx, rep, t] = ranks[m_idx]
-            
-            # Inter-Method Spearman Correlation (Across repeats)
-            for t in range(N_iters):
-                gp_vals = y_best_store[k][test, 0, :, t, 0].numpy()
-                pfn_vals = y_best_store[k][test, 1, :, t, 0].numpy()
-                rand_vals = y_best_store[k][test, 2, :, t, 0].numpy()
-                
-                # Compute correlation. Catch NaNs if vectors have 0 variance
-                rho_gp_pfn, _ = spearmanr(gp_vals, pfn_vals)
-                rho_gp_rand, _ = spearmanr(gp_vals, rand_vals)
-                rho_pfn_rand, _ = spearmanr(pfn_vals, rand_vals)
-                
-                metrics["spearman_gp_pfn"][test, k, t] = rho_gp_pfn if not np.isnan(rho_gp_pfn) else 0.0
-                metrics["spearman_gp_rand"][test, k, t] = rho_gp_rand if not np.isnan(rho_gp_rand) else 0.0
-                metrics["spearman_pfn_rand"][test, k, t] = rho_pfn_rand if not np.isnan(rho_pfn_rand) else 0.0
 
     # Save metrics
-    save_path = os.path.join(base_save_dir, "baseline_metrics.pt")
+    save_path = os.path.join(base_save_dir_2, "metrics_store.pt")
     torch.save(metrics, save_path)
     print(f"Metrics evaluated and saved successfully to {save_path}.")
 
 if __name__ == "__main__":
     # Substitute with your actual base_save_dir
-    base_dir = "results_LS_150/run_20260728_122058" 
-    compute_comparative_metrics(base_dir)
+    base_dir_1 = "results_LS_BO_PFN_GP/run_N_50_D_1" 
+    base_dir_2 = "results_LS_BO_ATR_PFN/run_N_50_D_1_mode_k4" 
+    compute_comparative_metrics(base_dir_1, base_dir_2)
